0e7fa191205a2f30a75cd62e004624c0e55793fe
nduan <154305664@qq.com>
2019-01-09 15:58:57 +0800

    add a bat file to debug

---
+++ jenkinsBat/QX4updatewebuiwar.bat
@@ -0,0 +1,28 @@
+rem @echo off
+rem copy 64bit web client war to tomcat
+
+@echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+:: Your codes should start from the following line
+
+xcopy /q /y /f "c:\lswebengine\web\webuiclientservlet\webclient.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.0\webapps\"
+timeout /T 15
+
+
